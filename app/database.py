0744dc797433aa3c019169ee554a75d6ad24d8a3
sylarcp <sylarcp@udel.edu>
2016-06-01 15:01:09 -0400

    add database.py

---
+++ app/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+engine0 = create_engine('postgresql://gui:AniTa08@128.175.112.125/anita_0116d', convert_unicode=True)
+engine1 = create_engine('postgresql://gui:AniTa08@128.175.112.80/anita_0105d', convert_unicode=True)
+engine2 = create_engine('postgresql://gui:AniTa08@128.175.112.80/anita_0106d', convert_unicode=True)
+engine3 = create_engine('postgresql://gui:AniTa08@128.175.112.58/anita_0105d', convert_unicode=True)
+engine4 = create_engine('postgresql://gui:AniTa08@128.175.112.58/anita_0106d', convert_unicode=True)
+db_session0 = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine0))
+db_session1 = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine1))
+db_session2 = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine2))
+db_session3 = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine3))
+db_session4 = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine4))
+Base = declarative_base()
+# Base.querys=[]
+# Base.querys.append(db_session.query_property())
+# Base.querys.append(db_session2.query_property())
+Base.query_128_175_112_125_anita_0116d = db_session0.query_property()
+Base.query_128_175_112_80_anita_0105d = db_session1.query_property()
+Base.query_128_175_112_80_anita_0106d = db_session2.query_property()
+Base.query_128_175_112_58_anita_0105d = db_session1.query_property()
+Base.query_128_175_112_58_anita_0106d = db_session2.query_property()
+
+# engine0 seems can be any database, does not affect the Base.query we use.
+Base.metadata.bind=engine0
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+	import app.models
+    # Base.metadata.create_all(bind=engine)
+    #autoload the database
+    # Base.metadata.reflect(bind=engine)
