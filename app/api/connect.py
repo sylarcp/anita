7aa7d2b7f6a26c5bb49b5c963730ca330b92b13b
sylarcp <sylarcp@udel.edu>
2016-06-03 16:10:14 -0400

    can connect to new database

--- app/api/connect.py
+++ app/api/connect.py
@@ -9,10 +9,28 @@ from app.database import Base
 
 @api.route('/connect/<ip>/<db>')
 def connect(ip,db):
-    engine = create_engine('postgresql://gui:AniTa08@128.175.112.58/anita_0107d', convert_unicode=True)
+    engine = create_engine('postgresql://gui:AniTa08@' + ip.replace('_','.') +'/' + db, convert_unicode=True)
     db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
-    Base.query_128_175_112_58_anita_0107d = db_session.query_property()
-    return 'done'
+    query_ip_db= 'query_' + ip  + '_' + db
+    if query_ip_db == 'query_128_175_112_125_anita_0116d':
+        Base.query_128_175_112_125_anita_0116d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_58_anita_0105d':
+        Base.query_128_175_112_58_anita_0105d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_58_anita_0106d':
+        Base.query_128_175_112_58_anita_0106d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_58_anita_0107d':
+        Base.query_128_175_112_58_anita_0107d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_58_anita_0108d':
+        Base.query_128_175_112_58_anita_0108d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_80_anita_0105d':
+        Base.query_128_175_112_80_anita_0105d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_80_anita_0106d':
+        Base.query_128_175_112_80_anita_0106d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_80_anita_0107d':
+        Base.query_128_175_112_80_anita_0107d= db_session.query_property()
+    elif query_ip_db == 'query_128_175_112_80_anita_0108d':
+        Base.query_128_175_112_80_anita_0108d= db_session.query_property()
+    return 'success'
 
 # def init_db():
 # 	import app.models
