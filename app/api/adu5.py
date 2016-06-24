ad8109df8d2ab2f5f86ebe97d183a781ac65c98e
Victor Arriaza <varriaza@udel.edu>
2016-06-24 12:00:38 -0400

    adu5 pat, sat and vtg connected and velocity working

--- app/api/adu5.py
+++ app/api/adu5.py
@@ -3,43 +3,38 @@ from flask.ext.login import LoginManager, current_user
 from . import api
 from app.models import Adu5_pat, Adu5_vtg, Adu5_sat
 
+@api.route('/<ip_db>/adu5_pat/nbufs')
+def get_adu5_pat_nbufs(ip_db):
+    adu5_pats =getattr(Adu5_pat,ip_db).limit(1000).all()
+    return jsonify({'adu5_pat_nbufs': [item.nbuf for item in adu5_pats], 'adu5_pat_nows': [item.now for item in adu5_pats]})
 
-@api.route('/<ip_db>/adu5/nbufs')
-def get_adu5_nbufs(ip_db):
-    # print session['ip']
-    # print session['db']
-    # print session['ip_db']
-    adu5s =getattr(Adu5_pat,ip_db).limit(1000).all()
-    #adu5_sats =getattr(Adu5_sat,ip_db).limit(1000).all()
-    #adu5_vtgs =getattr(Adu5_vtg,ip_db).limit(1000).all()
-    return jsonify({'adu5_nbufs': [item.nbuf for item in adu5s], 'adu5_nows': [item.now for item in adu5s]})
-    #return jsonify({'mon_nbufs': [item.nbuf for item in mons], 'mon_nows': [item.now for item in mons]})
-    # return jsonify({'hk': [item.now&mask for item in hks]})
+@api.route('/<ip_db>/adu5_vtg/nbufs')
+def get_adu5_vtg_nbufs(ip_db):
+    adu5_vtgs =getattr(Adu5_vtg,ip_db).limit(1000).all()
+    return jsonify({'adu5_vtg_nbufs': [item.nbuf for item in adu5_vtgs], 'adu5_vtg_nows': [item.now for item in adu5_vtgs]})
+
+@api.route('/<ip_db>/adu5_sat/nbufs')
+def get_adu5_sat_nbufs(ip_db):
+    adu5_sats =getattr(Adu5_sat,ip_db).limit(1000).all()
+    return jsonify({'adu5_sat_nbufs': [item.nbuf for item in adu5_sats], 'adu5_sat_nows': [item.now for item in adu5_sats]})
 
 
 
-@api.route('/<ip_db>/adu5/<nbuf>')
-def get_adu5(ip_db, nbuf):
-    #print ""
-    #print getattr(Adu5_vtg,ip_db).first().nbuf
+@api.route('/<ip_db>/adu5_pat/<nbuf>')
+def get_adu5_pat(ip_db, nbuf):
     adu5_pat =getattr(Adu5_pat,ip_db).filter_by(nbuf=nbuf).first()
+    return jsonify({'pat': adu5_pat.to_json()})
+
+@api.route('/<ip_db>/adu5_sat/<nbuf>')
+def get_adu5_sat(ip_db, nbuf):
     adu5_sat =getattr(Adu5_sat,ip_db).filter_by(nbuf=nbuf).first()
+    return jsonify({'sat': adu5_sat.to_json()})
+
+@api.route('/<ip_db>/adu5_vtg/<nbuf>')
+def get_adu5_vtg(ip_db, nbuf):
+#    print ""
+#    print getattr(Adu5_vtg,ip_db).first().nbuf
     adu5_vtg =getattr(Adu5_vtg,ip_db).filter_by(nbuf=nbuf).first()
-    json_comment={}
-    if adu5_pat == None:
-        json_comment['pat']=''
-    else:
-        json_comment['pat']=adu5_pat.to_json()
+    return jsonify({'vtg': adu5_vtg.to_json()})
 
-    if adu5_vtg == None:
-        json_comment['vtg']=''
-    else:
-        json_comment['vtg']=adu5_vtg.to_json()
-
-    if adu5_sat == None:
-        json_comment['sat']=''
-    else:
-        json_comment['sat']=adu5_sat.to_json()
-    # return jsonify({'pat':adu5_pat.to_json(), 'vtg':adu5_vtg.to_json(), 'sat':adu5_sat.to_json()})
-    return jsonify(json_comment)
 
