d6065177f6ccb5b3af186b320a9d15c1d71e22cd
mcollins12321 <mcollins12321@gmail.com>
2016-07-08 14:27:50 -0400

    added turf.py and TURF Monitor table on trigmon page is recieving data

---
+++ app/api/turf.py
@@ -0,0 +1,30 @@
+from flask import jsonify, request, g, abort, url_for, current_app, session
+from flask.ext.login import LoginManager, current_user
+from . import api
+from app.models import Turf
+
+# Primary key list: get the turf now list
+
+@api.route('/<ip_db>/turf/nbufs/<start_time>')
+def get_turf_nbufs(ip_db, start_time):
+    turfs =getattr(Turf,ip_db).with_entities(Turf.nbuf, Turf.now, Turf.time).filter(Turf.time>start_time).order_by(Turf.now).limit(200).all()
+    return jsonify({'turf_nbufs': [item.nbuf for item in turfs], 'turf_nows': [item.now for item in turfs], 'turf_times': [item.time for item in turfs]})
+
+# get the length of turf now list
+
+
+@api.route('/<ip_db>/turf/count')
+def get_turf_count(ip_db):
+    count =getattr(Turf,ip_db).count()
+    # could not return long type, so use str()
+    return str(count)
+    # return jsonify({'turf': [item.now&mask for item in turfs]})
+
+# get a tuple of Turf table
+
+
+@api.route('/<ip_db>/turf/<nbuf>')
+def get_turf(ip_db, nbuf):
+
+    turf =getattr(Turf,ip_db).filter_by(nbuf=nbuf).first()
+    return jsonify({'turf': turf.to_json()})
