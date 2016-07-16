fd9f79c9a17ac166e2eb16daf4c21e8f3e9f2d5d
sylarcp <sylarcp@udel.edu>
2016-07-16 10:27:43 -0500

    test

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-<<<<<<< HEAD
 import os
 import psycopg2
 import urlparse
