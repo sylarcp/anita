0a11075857cb3b8861c082408dcade426e51a16a
sylarcp <sylarcp@udel.edu>
2016-07-20 09:46:41 -0500

    minor change

--- app/api/connect.py
+++ app/api/connect.py
@@ -22,6 +22,7 @@ def connect(ip,db):
         query_ip_db= 'query_' + ip  + '_' + db
         #  query_ip_db is a string and should be Base's attribute. setattr() is perfect for this job
         setattr(Base,query_ip_db, db_session.query_property())
+        print '------', getattr(Base,ip_db)
         # flash("Database connect Successful", 'success') # flash message not working, why?
         return 'success'
     except:
