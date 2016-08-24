c5789f6a7056d085d9c0d718b2c78dd5d7eab94b
sylarcp <sylarcp@udel.edu>
2016-08-24 15:06:41 -0400

    dialog

--- app/api/history.py
+++ app/api/history.py
@@ -11,13 +11,19 @@ def get_history(ip_db, table_name, column_name, start_time, end_time):
         # dict = {'Hd':Hd, 'Wv':Wv, 'Hk':Hk, 'Mon':Mon, 'Adu5_sat':Adu5_sat, 'Adu5_vtg':Adu5_vtg, 'Adu5_pat':Adu5_pat, 'Sshk':Sshk, 'Turf':Turf, 'Hk_surf':Hk_surf}
         diction = {'hd':Hd, 'wv':Wv, 'hk':Hk, 'mon':Mon, 'adu5_sat':Adu5_sat, 'adu5_vtg':Adu5_vtg, 'adu5_pat':Adu5_pat}
         table = diction[table_name]
-        print table
-        results =getattr(table,ip_db).with_entities(getattr(table,column_name), table.time).filter(table.time>=start_time, table.time<=end_time).order_by(table.time).all()
+        if '_' in column_name:
+            splited = column_name.split('-')
+            column_name=splited[0]
+            column_id=int(splited[1])
+            print column_name, column_id
+            results =getattr(table,ip_db).with_entities(getattr(table,column_name), table.time).filter(table.time>=start_time, table.time<=end_time).order_by(table.time).all()
+            print [[result.time, getattr(result, column_name)[column_id]] for result in results]
+            return jsonify({'data':[[10000*result.time ,getattr(result, column_name)[column_id]] for result in results]})
+        else:
+            results =getattr(table,ip_db).with_entities(getattr(table,column_name), table.time).filter(table.time>=start_time, table.time<=end_time).order_by(table.time).all()
         # print results
-        print [[result.time, getattr(result, column_name)] for result in results]
-        return jsonify({'data':[getattr(result, column_name) for result in results]})
-        # return jsonify({ 'values':[getattr(result, column_name) for result in results], 'times':[result.time for result in results]});
-        # return jsonify({'hk_nbufs': [item.nbuf for item in hks], 'hk_nows': [item.now for item in hks], 'hk_times': [item.time for item in hks]})
+            print [[result.time, getattr(result, column_name)] for result in results]
+            return jsonify({'data':[[10000*result.time ,getattr(result, column_name)] for result in results]})
     except BaseException as error:
         print('Invalid request: {}', format(error))
         return jsonify({})
