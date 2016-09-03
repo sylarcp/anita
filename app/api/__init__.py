832cac2807108eb09d2565b51248ec9b3624775e
Victor Arriaza <varriaza@udel.edu>
2016-09-03 13:22:06 -0400

    Re-added changes lost from revert

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,4 @@
 
 from flask import Blueprint
 api = Blueprint('api', __name__)
-from . import connect, hd, slow, wv, hk, adu5, mon, history, turf, hk_surf, sshk
+from . import connect, hd, slow, wv, hk, adu5, mon, history, turf, hk_surf, sshk, g12
