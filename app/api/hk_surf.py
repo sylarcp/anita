8a83e8c53c10ce71d9369032334c1048bd1d64f9
sylarcp <sylarcp@udel.edu>
2016-12-05 15:19:58 -0500

    some minor change on hk surf

--- app/api/hk_surf.py
+++ app/api/hk_surf.py
@@ -16,30 +16,29 @@ def get_hk_surf_nbufs(ip_db, offset):
         # hk_surfs =getattr(Hk_surf,ip_db).with_entities(Hk_surf.nbuf, Hk_surf.now, Hk_surf.time).filter(Hk_surf.now>offset).order_by(Hk_surf.now).limit(200).all()
         # return jsonify({'hk_surf_nbufs': [item.nbuf for item in hk_surfs], 'hk_surf_nows': [item.now for item in hk_surfs], 'hk_surf_times': [item.time for item in hk_surfs]})
     except BaseException as error:
-        print('Invalid request: {}',format(error))
+        print("Invalid request: {'turf_nbufs':[], 'turf_nows': [],  'turf_times': []}",format(error))
         return jsonify({})
-# get the length of hk_surf now list
+# # get the length of hk_surf now list
 
 
-@api.route('/<ip_db>/hk_surf/count')
-def get_hk_surf_count(ip_db):
-    try:
-        count =getattr(Hk_surf,ip_db).count()
-        # could not return long type, so use str()
-        return str(count)
-        # return jsonify({'hk_surf': [item.now&mask for item in hk_surfs]})
-    except BaseException as error:
-        print('Invalid request: {}',format(error))
-        return jsonify({})
+# @api.route('/<ip_db>/hk_surf/count')
+# def get_hk_surf_count(ip_db):
+#     try:
+#         count =getattr(Hk_surf,ip_db).count()
+#         # could not return long type, so use str()
+#         return str(count)
+#         # return jsonify({'hk_surf': [item.now&mask for item in hk_surfs]})
+#     except BaseException as error:
+#         print('Invalid request: {}',format(error))
+#         return jsonify({})
 # get a tuple of Hk_surf table
 
 
 @api.route('/<ip_db>/hk_surf/<nbuf>')
-@cache.cached(timeout=3600)
 def get_hk_surf(ip_db, nbuf):
     try:
         hk_surf =getattr(Hk_surf,ip_db).filter_by(nbuf=nbuf).first()
         return jsonify({'hk_surf': hk_surf.to_json()})
     except BaseException as error:
-        print('Invalid request: {}',format(error))
+        print('Invalid request: {"hk_surf": {}.to_json()}',format(error))
         return jsonify({})
--- app/api/turf.py
+++ app/api/turf.py
@@ -16,7 +16,7 @@ def get_turf_nbufs(ip_db, offset):
         # turfs =getattr(Turf,ip_db).with_entities(Turf.nbuf, Turf.now, Turf.time).filter(Turf.now>offset).order_by(Turf.now).limit(200).all()
         # return jsonify({'turf_nbufs': [item.nbuf for item in turfs], 'turf_nows': [item.now for item in turfs], 'turf_times': [item.time for item in turfs]})
     except BaseException as error:
-        print('Invalid request: {}',format(error))
+        print("Invalid request: {'turf_nbufs':[], 'turf_nows': [],  'turf_times': []}",format(error))
         return jsonify({})
 # get the length of turf now list
 
