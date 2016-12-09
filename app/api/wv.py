b15ce5a6f9e56e21054d16ab5ba7ca4b8371e1fa
sylarcp <sylarcp@udel.edu>
2016-12-08 20:14:51 -0500

    event average

--- app/api/wv.py
+++ app/api/wv.py
@@ -45,9 +45,54 @@ def get_wvs(ip_db, evnum):
         json_comment={}
         wvs =getattr(Wv,ip_db).filter_by(evnum=evnum).order_by(Wv.id).all()
         json_comment['evnum'] = wvs[0].evnum
+        # print type(wvs[0].evnum)
+        # print type(wvs[0].cal[0])
+        # print type(wvs[0].to_json()['cal'][0])
         for wv in wvs:
             json_comment[wv.id]=wv.to_json()
             # json_comment[wv.id + 100]=wv.to_json()
+        return jsonify(json_comment)
+    except BaseException as error:
+        print('An exception occurred: {}'.format(error))
+        return jsonify
+
+#get averaged 40 waveforms for a time range.
+@api.route('/<ip_db>/avgwv/<int:starttime>/<int:endtime>/<int:limitNum>')
+# @cache.cached(timeout=3600)
+def get_avgwvs(ip_db, starttime, endtime, limitNum):
+    try:
+        engine_ip_db = ip_db.replace('query','session')
+        engine = getattr(Wv,engine_ip_db)
+        evnums = engine.execute('SELECT evnum FROM rf WHERE time>=' + str(starttime) + ' AND time<=' + str(endtime) + 'ORDER BY time DESC LIMIT '+ str(limitNum) + ';').fetchall()
+        
+        json_comment={}
+        count = 0
+        # print evnums
+        for evnum in evnums:
+            wvs =getattr(Wv,ip_db).filter_by(evnum=evnum[0]).order_by(Wv.id).all()
+            # print wvs
+            if count == 0:
+                last_evnum = evnum[0]
+            for wv in wvs:
+                if wv.id not in json_comment:
+                    json_comment[wv.id]=wv.to_json()
+                else:
+                    sumed_wv = json_comment[wv.id]
+                    current_wv = wv.to_json()
+                    new_wv = [x + y for x, y in zip(sumed_wv['cal'], current_wv['cal'])]
+                    sumed_wv['cal'] = new_wv
+                    json_comment[wv.id] = sumed_wv
+                # json_comment[wv.id + 100]=wv.to_json()
+            count += 1
+        first_evnum = evnum[0]
+        if count:
+            for id in json_comment:
+                # print id
+                # print json_comment[id]
+                json_comment[id]['cal'] = [sumed_mv/count for sumed_mv in json_comment[id]['cal']]
+            json_comment['evnum'] = last_evnum
+        json_comment['count'] = count
+        json_comment['first_evnum'] = first_evnum
         return jsonify(json_comment)
     except BaseException as error:
         print('An exception occurred: {}'.format(error))
